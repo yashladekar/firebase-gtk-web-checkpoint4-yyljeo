aa893e8058d60bfdc025cf35382687e409a83aa6
yashladekar <yashladekar002@gmail.com>
2021-03-21 17:41:57 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# firebase-gtk-web-checkpoint4-2au8kb
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/firebase-gtk-web-checkpoint4-2au8kb)
---
+++ index.js
@@ -0,0 +1,147 @@
+// Import stylesheets
+import "./style.css";
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import * as firebase from "firebase/app";
+
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+
+import * as firebaseui from "firebaseui";
+
+// Document elements
+const startRsvpButton = document.getElementById("startRsvp");
+const guestbookContainer = document.getElementById("guestbook-container");
+
+const form = document.getElementById("leave-message");
+const input = document.getElementById("message");
+const guestbook = document.getElementById("guestbook");
+const numberAttending = document.getElementById("number-attending");
+const rsvpYes = document.getElementById("rsvp-yes");
+const rsvpNo = document.getElementById("rsvp-no");
+
+var rsvpListener = null;
+var guestbookListener = null;
+
+async function main() {
+ // Add Firebase project configuration object here
+  var firebaseConfig = {
+    apiKey: "AIzaSyAG4TctI3NZymEN5wo8nyZC8pG7lq9d29Q",
+    authDomain: "web-codelab-a5b4d.firebaseapp.com",
+    projectId: "web-codelab-a5b4d",
+    storageBucket: "web-codelab-a5b4d.appspot.com",
+    messagingSenderId: "153017746680",
+    appId: "1:153017746680:web:0bdc7914d5a8376b5a270d"
+  };
+
+   firebase.initializeApp(firebaseConfig);
+  var firebaseConfig = {};
+
+  // Make sure Firebase is initilized
+  try {
+    if (firebaseConfig && firebaseConfig.apiKey) {
+      firebase.initializeApp(firebaseConfig);
+    }
+    let app = firebase.app();
+  } catch (e) {
+    document.getElementById("app").innerHTML =
+      "<h1>Welcome to the Codelab! Add your Firebase config object to <pre>/index.js</pre> and refresh to get started</h1>";
+    console.log(e);
+    throw new Error(
+      "Welcome to the Codelab! Add your Firebase config object from the Firebase Console to `/index.js` and refresh to get started"
+    );
+  }
+
+  // FirebaseUI config
+  const uiConfig = {
+    credentialHelper: firebaseui.auth.CredentialHelper.NONE,
+    signInOptions: [
+      // Email / Password Provider.
+      firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ],
+    callbacks: {
+      signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+        // Handle sign-in.
+        // Return false to avoid redirect.
+        return false;
+      }
+    }
+  };
+
+  const ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+  // Listen to RSVP button clicks
+  startRsvpButton.addEventListener("click", () => {
+    if (firebase.auth().currentUser) {
+      // User is signed in; allows user to sign out
+      firebase.auth().signOut();
+    } else {
+      // No user is signed in; allows user to sign in
+      ui.start("#firebaseui-auth-container", uiConfig);
+    }
+  });
+
+  // Listen to the current Auth state
+  firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+      startRsvpButton.textContent = "LOGOUT";
+      // Show guestbook to logged-in users
+      guestbookContainer.style.display = "block";
+      subscribeGuestbook();
+    } else {
+      startRsvpButton.textContent = "RSVP";
+      // Hide guestbook for non-logged-in users
+      guestbookContainer.style.display = "none";
+      unsubscribeGuestbook();
+    }
+  });
+
+  // Listen to the form submission
+  form.addEventListener("submit", e => {
+    // Prevent the default form redirect
+    e.preventDefault();
+    // Write a new message to the database collection 'guestbook'
+    firebase
+      .firestore()
+      .collection("guestbook")
+      .add({
+        text: input.value,
+        timestamp: Date.now(),
+        name: firebase.auth().currentUser.displayName,
+        userId: firebase.auth().currentUser.uid
+      });
+    // clear message input field
+    input.value = "";
+    // Return false to avoid redirect
+    return false;
+  });
+}
+main();
+
+// Listen to guestbook updates
+function subscribeGuestbook() {
+  // Create query for messages
+  guestbookListener = firebase
+    .firestore()
+    .collection("guestbook")
+    .orderBy("timestamp", "desc")
+    .onSnapshot(snaps => {
+      // Reset page
+      guestbook.innerHTML = "";
+      // Loop through documents in database
+      snaps.forEach(doc => {
+        // Create an HTML entry for each document and add it to the chat
+        const entry = document.createElement("p");
+        entry.textContent = doc.data().name + ": " + doc.data().text;
+        guestbook.appendChild(entry);
+      });
+    });
+}
+
+// Unsubscribe from guestbook updates
+function unsubscribeGuestbook() {
+  if (guestbookListener != null) {
+    guestbookListener();
+    guestbookListener = null;
+  }
+}
